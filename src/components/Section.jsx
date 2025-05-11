4013d65e1cd383d568b79f2f9fb5540f7b02cc0d
Ionathan Sideras <iona8ansideras@gmail.com>
2025-05-11 16:59:48 +0300

    commit benefits

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,28 +1,28 @@
 import SectionSvg from "../assets/svg/SectionSvg";
 
-export default function Section({
+const Section = ({
     className,
     id,
     crosses,
     crossesOffset,
     customPaddings,
     children,
-}) {
+}) => {
     return (
-        <section
+        <div
             id={id}
             className={`
-        relative 
-        ${
-            customPaddings ||
-            `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
-        } 
-        ${className || ""}`}
+      relative 
+      ${
+          customPaddings ||
+          `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
         >
             {children}
 
-            <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10"></div>
-            <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10"></div>
+            <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
+            <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
             {crosses && (
                 <>
@@ -30,11 +30,12 @@ export default function Section({
                         className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
                             crossesOffset && crossesOffset
                         } pointer-events-none lg:block xl:left-10 right-10`}
-                    ></div>
-
+                    />
                     <SectionSvg crossesOffset={crossesOffset} />
                 </>
             )}
-        </section>
+        </div>
     );
-}
+};
+
+export default Section;
