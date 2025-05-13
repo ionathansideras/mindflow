101acd2eae1c54c40b49cc2d19bcbd97d9657449
Ionathan Sideras <iona8ansideras@gmail.com>
2025-05-13 17:32:43 +0300

    commit section collaboration

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import ButtonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, onClick, children, px, white }) => {
     const classes = twMerge(
-        `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+        `button relative inline-flex cursor-pointer items-center justify-center h-11 transition-colors hover:text-color-1 ${
             px || "px-7"
         } ${white ? "text-n-8" : "text-n-1"}`,
         className
